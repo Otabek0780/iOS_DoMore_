09a310b6efee909c501be51e4aa1201b3ffe44c5
Otabek0780 <mrdunacdtm@gmail.com>
2021-02-02 21:59:18 +0900

    Initial Commit

---
+++ iOS_DoMore_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_DoMore_
+//
+//  Created by kim jong moon on 02/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
